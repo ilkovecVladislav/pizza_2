2b01ba16ea03cc42723344562cf671db657a06af
Uladzislau Ilkavets <uladzislau_ilkavets@marpaihealth.com>
2021-01-10 18:04:48 +0300

    updated ci

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import './index.css';
 
 Sentry.init({
   dsn: 'https://2a79de97b9bb43d2a2aad9bc8f64f8fe@o502735.ingest.sentry.io/5585386',
+  release: process.env.REACT_APP_SENTRY_RELEASE,
   autoSessionTracking: true,
   integrations: [new Integrations.BrowserTracing()],
 
